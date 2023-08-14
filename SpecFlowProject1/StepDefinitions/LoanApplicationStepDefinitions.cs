8e27b14e7144e0239d37b62e78247794752591ab
Munib Hassan <munib@practicepanther.com>
2023-08-15 00:18:47 +0500

    Add project files.

---
+++ SpecFlowProject1/StepDefinitions/LoanApplicationStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject1.StepDefinitions
+{
+    [Binding]
+    public class LoanApplicationStepDefinitions
+    {
+        [Given(@"John is an active ParaBank customer")]
+        public void GivenJohnIsAnActiveParaBankCustomer()
+        {
+
+        }
+
+        [When(@"they apply for a (\d+) dollar loan")]
+        public void WhenTheyApplyForADollarLoan(int loanAmount)
+        {
+
+        }
+
+        [Then(@"the loan application is (approved | denied)")]
+        public void ThenTheLoanApplicationIsApproved()
+        {
+
+        }
+
+        [When(@"their monthly income is (.*)")]
+        public void WhenTheirMonthlyIncomeIs(int p0)
+        {
+
+        }
+    }
+}
